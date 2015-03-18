7beb44f052c2a92614d773718e38b534466fd455
ELIOHENKI <legenkiyes@gmail.com>
2015-03-18 22:34:18 +0300

    Init repo

---
+++ SelfSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SelfSite.Startup))]
+namespace SelfSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
