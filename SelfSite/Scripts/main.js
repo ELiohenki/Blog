7530fc3adc643a11ffa8f0b8c5eecd4c7b9a386b
ELIOHENKI <legenkiyes@gmail.com>
2015-03-19 22:54:50 +0300

    require.js

---
+++ SelfSite/Scripts/main.js
@@ -0,0 +1,20 @@
+﻿requirejs.config({
+    "baseUrl": "js/lib",
+    "paths": {
+        "app": "../app"
+    },
+    "shim": {
+        "jquery.alpha": ["jquery"],
+        "jquery.beta": ["jquery"]
+    }
+});
+require.config({
+    // relative url from where modules will load
+    baseUrl: "Scripts/",
+    paths: {
+        "jquery": "jquery-1.10.2"
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["app/main"]);
