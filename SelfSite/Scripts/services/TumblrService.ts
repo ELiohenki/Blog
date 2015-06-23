b84206de35c8fb7647d1b5b92aaaf19475295da8
unknown <liohenkie@PC15.dana>
2015-06-23 16:55:58 +0300

    Half way start testing my services

--- SelfSite/Scripts/services/TumblrService.ts
+++ SelfSite/Scripts/services/TumblrService.ts
@@ -1,1 +1,16 @@
-﻿ 
+﻿module Selfsite {
+    class tumblrService {
+        getPosts;
+
+        constructor($http) {
+            this.getPosts = function (offset: number, count: number) : any {
+                var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
+                return $http.get(url).then(response => JSON.parse(response).response.posts.slice(offset, offset + count));
+            };
+        }
+    }
+
+    angular
+        .module("selfsite")
+        .service("tumblrService", ['$http', tumblrService]);
+}
--- SelfSiteTests/ng-tests/TestOfTest.js
+++ SelfSiteTests/ng-tests/TestOfTest.js
@@ -1,7 +1,21 @@
 ﻿describe('sorting the list of users', function () {
+    var tumblrServiceObj, scope;
+
+    beforeEach(module("selfsite"));
+
+    beforeEach(inject(function (tumblrService) {
+        tumblrServiceObj = tumblrService;
+        scope = {};
+    }));
+
+    it('should have scope defined', function () {
+        expect(scope).toBeDefined();
+    });
+
     it('sorts in descending order by default', function () {
-        var users = ['jack', 'igor', 'jeff'];
-        var sorted = sortUsers(users);
-        expect(sorted).toEqual(['jeff', 'jack', 'igor']);
+        var data = tumblrServiceObj.getPosts(0, 10);
+        throw new EventException(data.toString());
+        expect(data).toNotEqual(null);
     });
 });
+
