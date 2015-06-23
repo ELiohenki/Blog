416aba3334b4042613f197ca971ae4731cdfec59
unknown <liohenkie@PC15.dana>
2015-06-23 20:45:51 +0300

    Almost get js tests working

--- SelfSite/Scripts/services/TumblrService.ts
+++ SelfSite/Scripts/services/TumblrService.ts
@@ -1,16 +1,26 @@
-﻿module Selfsite {
-    class tumblrService {
-        getPosts;
+﻿var tumblrService = function($http) {
+    this.getPosts = (offset: number, count: number): any => {
+        var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
+        return $http.get(url).then(response => JSON.parse(response).response.posts.slice(offset, offset + count));
+    };
+};
 
-        constructor($http) {
-            this.getPosts = function (offset: number, count: number) : any {
-                var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
-                return $http.get(url).then(response => JSON.parse(response).response.posts.slice(offset, offset + count));
-            };
+angular
+    .module("selfsite")
+    .service("tumblrService", ['$http', tumblrService]);
+
+var sampleSvc = function($window, modalSvc) {
+    this.showDialog = function(message, title) {
+        if (title) {
+            modalSvc.showModalDialog({
+                title: title,
+                message: message
+            });
+        } else {
+            $window.alert(message);
         }
-    }
+    };
+};
 
-    angular
-        .module("selfsite")
-        .service("tumblrService", ['$http', tumblrService]);
-}
+angular.module('services')
+    .service('sampleSvc', ['$window', 'modalSvc', sampleSvc]);
---
+++ SelfSiteTests/Scripts/ng-tests/TestOfTest.js
@@ -0,0 +1,75 @@
+﻿/// <reference path="../_references.js" />
+/// <reference path="../../../selfsite/Scripts/services/tumblrService.js" />
+
+describe('lalaala', function () {
+    var mockWindow, mockModalSvc, sampleSvcObj;
+    beforeEach(function () {
+        module(function ($provide) {
+            $provide.service('$window', function () {
+                this.alert = jasmine.createSpy('alert');
+            });
+            $provide.service('modalSvc', function () {
+                this.showModalDialog = jasmine.createSpy('showModalDialog');
+            });
+        });
+        module('services');
+    });
+
+    beforeEach(inject(function ($window, modalSvc, sampleSvc) {
+        mockWindow = $window;
+        mockModalSvc = modalSvc;
+        sampleSvcObj = sampleSvc;
+    }));
+
+    it('should show alert when title is not passed into showDialog', function () {
+        var message = "Some message";
+        sampleSvcObj.showDialog(message);
+
+        expect(mockWindow.alert).toHaveBeenCalledWith(message);
+        expect(mockModalSvc.showModalDialog).not.toHaveBeenCalled();
+    });
+
+    it('should show modal when title is passed into showDialog', function () {
+        var message = "Some message";
+        var title = "Some title";
+        sampleSvcObj.showDialog(message, title);
+
+        expect(mockModalSvc.showModalDialog).toHaveBeenCalledWith({
+            message: message,
+            title: title
+        });
+        expect(mockWindow.alert).not.toHaveBeenCalled();
+    });
+});
+
+
+describe('sorting the list of users', function () {
+    var tumblrServiceObj, $httpObj;
+
+    beforeEach(function () {
+        module(function ($provide) {
+            $provide.service('$http', function () {
+                var spy = jasmine.createSpy('get');
+                spy = spy.andCallFake(function (num) {
+                    return new function () {
+                        var then = function(asd) {};
+                    };
+                });;
+                this.get = spy;
+            });
+        });
+        module('selfsite');
+    });
+
+    beforeEach(inject(function ($http, tumblrService) {
+        tumblrServiceObj = $http;
+        tumblrServiceObj = tumblrService;
+    }));
+
+    it('sorts in descending order by default', function () {
+        var data = tumblrServiceObj.getPosts(0, 10);
+        throw new EventException(data.toString());
+        expect(data).toNotEqual(null);
+    });
+});
+
