b6f5b8920e1e530fdd2463826fffe042f59c72f4
unknown <liohenkie@PC15.dana>
2015-07-07 20:47:00 +0300

    Get real posts from tumblr

--- SelfSite/Scripts/services/TumblrService.js
+++ SelfSite/Scripts/services/TumblrService.js
@@ -1,24 +1,56 @@
-var tumblrService = function ($http) {
-    var getPosts = function () {
-        var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
-        var myDataPromise = $http.get(url);
-        return myDataPromise;
-    };
-    return { getPosts: getPosts };
-};
-angular.module("selfsite").service("tumblrService", ['$http', tumblrService]);
-var sampleSvc = function ($window, modalSvc) {
-    this.showDialog = function (message, title) {
-        if (title) {
-            modalSvc.showModalDialog({
-                title: title,
-                message: message
-            });
-        }
-        else {
-            $window.alert(message);
-        }
-    };
-};
-angular.module('services').service('sampleSvc', ['$window', 'modalSvc', sampleSvc]);
+var Selfsite;
+(function (Selfsite) {
+    var Tumblr;
+    (function (Tumblr) {
+        "use strict";
+        var TumblrService = (function () {
+            function TumblrService($http, $q) {
+                this.$http = $http;
+                this.$q = $q;
+            }
+            TumblrService.prototype.getPosts = function () {
+                var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
+                var myDataPromise = this.$http.get(url);
+                var deferred = this.$q.defer();
+                myDataPromise.success(function (result) {
+                    var retObject = JSON.parse(result.toString());
+                    var posts = new Array();
+                    retObject["response"]["posts"].forEach(function (postObj) {
+                        var post = new Selfsite.Post();
+                        post.body = postObj.trail.content;
+                        posts.push(post);
+                    });
+                    deferred.resolve(posts);
+                });
+                return deferred.promise;
+            };
+            TumblrService.prototype.getFilteredPosts = function (offset, fetch) {
+                var url = 'http://api.tumblr.com/v2/blog/eliohenki.tumblr.com/posts/text';
+                var myDataPromise = this.$http.get(url);
+                var deferred = this.$q.defer();
+                myDataPromise.success(function (result) {
+                    var retObject = JSON.parse(result.toString());
+                    var posts = new Array();
+                    retObject["response"]["posts"].forEach(function (postObj) {
+                        var post = new Selfsite.Post();
+                        post.body = postObj.body;
+                        post.header = postObj.title;
+                        post.id = postObj.id;
+                        post.time = postObj.date;
+                        post.url = postObj.post_url;
+                        posts.push(post);
+                    });
+                    posts = posts.slice(offset, offset + fetch - 1);
+                    deferred.resolve(posts);
+                });
+                return deferred.promise;
+            };
+            TumblrService.$inject = ["$http", "$q"];
+            return TumblrService;
+        })();
+        Tumblr.TumblrService = TumblrService;
+        ;
+        angular.module("selfsite").service("tumblrService", TumblrService);
+    })(Tumblr = Selfsite.Tumblr || (Selfsite.Tumblr = {}));
+})(Selfsite || (Selfsite = {}));
 //# sourceMappingURL=tumblrService.js.map
--- SelfSiteTests/Scripts/ng-tests/TestOfTest.js
+++ SelfSiteTests/Scripts/ng-tests/TestOfTest.js
@@ -1,48 +1,6 @@
 ﻿/// <reference path="../_references.js" />
 /// <reference path="../../../selfsite/Scripts/services/tumblrService.js" />
 
-describe('lalaala', function () {
-    var mockWindow, mockModalSvc, sampleSvcObj;
-    beforeEach(function () {
-        module(function ($provide) {
-            $provide.service('$window', function () {
-                this.alert = jasmine.createSpy('alert');
-            });
-            $provide.service('modalSvc', function () {
-                this.showModalDialog = jasmine.createSpy('showModalDialog');
-            });
-        });
-        module('services');
-    });
-
-    beforeEach(inject(function ($window, modalSvc, sampleSvc) {
-        mockWindow = $window;
-        mockModalSvc = modalSvc;
-        sampleSvcObj = sampleSvc;
-    }));
-
-    it('should show alert when title is not passed into showDialog', function () {
-        var message = "Some message";
-        sampleSvcObj.showDialog(message);
-
-        expect(mockWindow.alert).toHaveBeenCalledWith(message);
-        expect(mockModalSvc.showModalDialog).not.toHaveBeenCalled();
-    });
-
-    it('should show modal when title is passed into showDialog', function () {
-        var message = "Some message";
-        var title = "Some title";
-        sampleSvcObj.showDialog(message, title);
-
-        expect(mockModalSvc.showModalDialog).toHaveBeenCalledWith({
-            message: message,
-            title: title
-        });
-        expect(mockWindow.alert).not.toHaveBeenCalled();
-    });
-});
-
-
 describe('sorting the list of users', function () {
     var tumblrServiceObj, $httpObj, deferred, $rootScopeObj;
 
