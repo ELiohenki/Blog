bf9e882b88025ef2c60fd7d8d97865d9750d9a2f
unknown <liohenkie@PC15.dana>
2015-07-21 15:09:58 +0300

    Add font-awesome.css, add some ui to main page. Fixed blog padding

--- SelfSite/App_Start/BundleConfig.cs
+++ SelfSite/App_Start/BundleConfig.cs
@@ -9,24 +9,25 @@ namespace SelfSite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/libs/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/libs/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/libs/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/libs/bootstrap/bootstrap.js",
+                      "~/Scripts/libs/bootstrap/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/bootstrap-theme.css"));
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/font-awesome.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/blogModule").Include(
                       "~/Scripts/blog/*.js"));
