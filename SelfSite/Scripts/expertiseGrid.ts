fad72104876d3ed098a18c6b4b71f7c6a4154852
unknown <liohenkie@PC15.dana>
2015-11-04 20:59:03 +0300

    adding expertise grid

---
+++ SelfSite/Scripts/expertiseGrid.ts
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    // init Isotope
+    var $container = $('.isotope').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function (itemElem) {
+                var weight = $(itemElem).find('.weight').text();
+                return parseFloat(weight.replace(/[\(\)]/g, ''));
+            }
+        }
+    });
